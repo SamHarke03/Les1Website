0626631a668b805fef124e285cdcfe443822b900
SamHarke <sam.harke@windesheim.nl>
2024-02-13 12:20:57 +0100

    Contact page and Menu bar

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+let cors = require('cors')
+app.use(cors());
+app.use(express.json());
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send("Hallo wereld!");
+});
+
+
+app.post('/form', (req, res) => {
+
+    console.log(req.body);
+
+    let email = req.body.email;
+    let phone  = req.body.phone;
+
+    res.json({email: email, phone: phone});
+});
+
+app.listen(port, () => console.log(`Data API listening on port ${port}!`))
